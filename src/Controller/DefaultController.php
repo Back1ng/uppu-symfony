f967127ccd46246b90c435d90d5c1513d398fb42
Back1ng <anatolyu128@gmail.com>
2020-07-17 17:30:28 +0500

    change controller to work with file

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,54 +2,11 @@
 namespace App\Controller;
 
 use App\Entity\File;
-use App\Form\FileType;
-use App\Service\DirectoryManager;
-use App\Service\FileUploader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    /**
-     * @Route("/", name="index")
-     */
-    public function index(Request $request, Filesystem $filesystem, $uploadedDir)
-    {
-        $entityManager = $this->getDoctrine()->getManager();
-
-        $form = $this->createForm(FileType::class);
-
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $files = $form->getData();
-
-            foreach ($files as $file) {
-                if (null === $file) {
-                    return new Response("", 404);
-                }
-
-                $uploadedFile = (new FileUploader())
-                    ->setDirectory($uploadedDir.'/'.date('Y-m-d'))
-                    ->setFile($file)
-                    ->setServerName($this->generateUniqueName())
-                    ->store();
-
-                $entityManager->persist($uploadedFile);
-                $entityManager->flush();
-
-                return $this->redirectToRoute("show_file", ['id' => $uploadedFile->getId()]);
-            }
-        }
-
-        return $this->render('default/index.html.twig', [
-            'form' => $form->createView()
-        ]);
-    }
-
     /**
      * @Route("/files", name="files")
      */
--- src/Controller/FileController.php
+++ src/Controller/FileController.php
@@ -3,13 +3,52 @@
 namespace App\Controller;
 
 use App\Entity\File;
+use App\Form\FileType;
+use App\Service\FileUploader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FileController extends AbstractController
 {
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(Request $request, Filesystem $filesystem, $uploadedDir)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $form = $this->createForm(FileType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $files = $form->getData();
+
+            foreach ($files as $file) {
+                if (null === $file) {
+                    return new Response("", 404);
+                }
+
+                $uploadedFile = (new FileUploader())
+                    ->setDirectory($uploadedDir.'/'.date('Y-m-d'))
+                    ->setFile($file)
+                    ->setServerName($this->generateUniqueName())
+                    ->store();
+
+                $entityManager->persist($uploadedFile);
+                $entityManager->flush();
+
+                return $this->redirectToRoute("show_file", ['id' => $uploadedFile->getId()]);
+            }
+        }
+
+        return $this->render('default/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
     /**
      * @Route("/file/{id}", name="show_file")
      */
